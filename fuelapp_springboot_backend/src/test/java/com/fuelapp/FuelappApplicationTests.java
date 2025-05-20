47544f481b5a4cc43e5f777b002d94d51b1bb5f8
DDJ-02 <149932599+DDJ-02@users.noreply.github.com>
2025-05-21 00:38:04 +0530

    added springboot backend

---
+++ fuelapp_springboot_backend/src/test/java/com/fuelapp/FuelappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fuelapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuelappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
