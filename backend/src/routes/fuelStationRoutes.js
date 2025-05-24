40510bdde8e72c82d49413cf42271c3676ed7c44
shenal-anthony <fonseka-im21039@stu.kln.ac.lk>
2025-05-24 22:26:47 +0530

    remove the old backend

--- backend/src/routes/fuelStationRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { registerFuelStation } = require('../controllers/fuelStationController');
-const { verifyToken, isFuelStationOwnerOrOperator } = require('../middleware/auth');
-
-router.post('/register', verifyToken, isFuelStationOwnerOrOperator, registerFuelStation);
-
-module.exports = router;
